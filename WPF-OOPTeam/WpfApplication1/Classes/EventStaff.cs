5bec3122af71bdc07ac85224c22d80d2338f814c
pmechkova <pmechkova@gmail.com>
2015-03-30 00:20:47 +0300

    Added abstract class EventStaff, IRequeredStaff.cs, class EventStaff, class RestaurantStaff.cs, class Cook, class Waiter, class DJ, class Singer

--- WPF-OOPTeam/WpfApplication1/Classes/EventStaff.cs
+++ WPF-OOPTeam/WpfApplication1/Classes/EventStaff.cs
@@ -1,47 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace Test
 {
-    public class EventStaff : Person
+    using System;
+    using System.Collections.Generic;
+
+    public abstract class EventStaff : Person
     {
-        private int dj;
-        private int animator;
-        private int singer;
-    
-        public int DJ
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
-        }
-
-        public int Singer
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
-        }
-
-        public int Animator
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
-        }
+       
     }
 }
